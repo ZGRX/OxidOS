d622c24ce66c1de631287ee1328eb3c5e3a0f3fd
jungle_ricey <1796962310@qq.com>
2026-04-16 17:21:06 +0800

    Initial commit: The birth of my OS"

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![no_std]// 不链接 Rust 标准库
+#![no_main]// 禁用所有 Rust 层级的入口点
+use core::panic::PanicInfo;
+// 这个函数将在 panic 时被调用
+#[panic_handler] // 不重整函数名
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    // 因为链接器会寻找一个名为 `_start` 的函数，所以这个函数就是入口点
+    // 默认命名为 `_start`
+    loop {}
+}
